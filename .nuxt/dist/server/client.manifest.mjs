e52b65bbfa36c0340b9e9f719b1f7e03cae8a9a8
ilan DARMON <ilan.darmon@etu.univ-grenoble-alpes.fr>
2026-01-29 21:26:26 +0100

    Updates

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,4 @@
+
+import { viteNodeFetch } from "file:///home/ilan/iddev-project/jeux_bienEtre_parenthese/app/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
+export default () => viteNodeFetch.getManifest()
+    
